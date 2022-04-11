4dcd5a6c8771d28fb21dcdf2d08ec7529db24818
coldmvm <marcus.miranda@gmail.com>
2022-04-11 12:39:44 -0300

    Update buildall.sh

--- buildall.sh
+++ buildall.sh
@@ -1,24 +1,30 @@
 #/bin/bash
 clear
 echo ""
-echo "Updating from GitHub..."
+echo "****************************************************************"
+echo "*                        SCRIPT STARTED                        *"
+echo "****************************************************************"
+echo ""
+echo "******************* Updating from GitHub..."
 git pull origin main
 echo ""
 
-echo "Building gmpack-forwarder..."
+echo "******************* Building gmpack-forwarder..."
 cd gmpack-forwarder
 make
 cd ..
 echo ""
 
-echo "Building gmpack-rcm..."
+echo "******************* Building gmpack-rcm..."
 cd gmpack-rcm
 make
 cd ..
 echo ""
 
-echo "Building gmpack-updater..."
+echo "******************* Building gmpack-updater..."
 make
 echo ""
 
-echo "DONE!"
+echo "****************************************************************"
+echo "*                       SCRIPT TERMINATED                      *"
+echo "****************************************************************"
