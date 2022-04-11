897c7ddbf67a9030f01cb80244089f812ca2cfe6
coldmvm <marcus.miranda@gmail.com>
2022-04-11 12:36:37 -0300

    Update buildall.sh

--- buildall.sh
+++ buildall.sh
@@ -1,10 +1,24 @@
 #/bin/bash
 clear
+echo ""
+echo "Updating from GitHub..."
 git pull origin main
+echo ""
+
+echo "Building gmpack-forwarder..."
 cd gmpack-forwarder
 make
 cd ..
+echo ""
+
+echo "Building gmpack-rcm..."
 cd gmpack-rcm
 make
 cd ..
+echo ""
+
+echo "Building gmpack-updater..."
 make
+echo ""
+
+echo "DONE!"
