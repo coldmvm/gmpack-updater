8d7e8e36e3320d4a4db87eb7a74c7c2b07577eb2
coldmvm <marcus.miranda@gmail.com>
2022-04-11 12:52:59 -0300

    Create cleanall.sh

---
+++ cleanall.sh
@@ -0,0 +1,28 @@
+#/bin/bash
+clear
+echo ""
+echo "****************************************************************"
+echo "*                        SCRIPT STARTED                        *"
+echo "****************************************************************"
+echo ""
+echo "******************* Cleaning environment..."
+make clean
+
+echo "cleaning gmpack-forwarder..."
+cd gmpack-forwarder
+rm -rf build
+rm gmpack-forwarder.elf
+rm gmpack-forwarder.nacp
+rm gmpack-forwarder.nro
+cd ..
+
+echo "cleaning gmpack-rcm..."
+cd gmpack-rcm
+rm -rf build
+rm -rf output
+
+
+echo ""
+echo "****************************************************************"
+echo "*                       SCRIPT TERMINATED                      *"
+echo "****************************************************************"
