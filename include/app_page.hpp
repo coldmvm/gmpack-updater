fedf3cc31c886f52645c6a69359d424409be9cb4
coldmvm <marcus.miranda@gmail.com>
2022-04-13 23:49:44 -0300

    remove CreateDownloadAllButton

--- include/app_page.hpp
+++ include/app_page.hpp
@@ -26,7 +26,6 @@ protected:
     brls::List* list;
     brls::Label* label;
     brls::ListItem* listItem;
-    void CreateDownloadAllButton();
     uint64_t GetCurrentApplicationId();
     u32 InitControlData(NsApplicationControlData** controlData);
     uint32_t GetControlData(u64 tid, NsApplicationControlData* controlData, u64& controlSize, std::string& name);
