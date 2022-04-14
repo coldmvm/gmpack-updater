0a37ee7f6cd2334700fa30b191888c043a49a2f8
coldmvm <marcus.miranda@gmail.com>
2022-04-14 13:25:32 -0300

    Revert commit.

--- source/main_frame.cpp
+++ source/main_frame.cpp
@@ -43,15 +43,12 @@ MainFrame::MainFrame() : TabFrame()
     if (!util::getBoolValue(hideStatus, "atmosphere"))
         this->addTab("menus/main/update_ams"_i18n, new AmsTab(nxlinks, erista, util::getBoolValue(hideStatus, "atmosphereentries")));
 
-<<<<<<< HEAD
-=======
 /*    if (!util::getBoolValue(hideStatus, "cfw"))
         this->addTab("menus/main/update_bootloaders"_i18n, new ListDownloadTab(contentType::bootloaders, nxlinks));
 
     if (!util::getBoolValue(hideStatus, "sigpatches"))
         this->addTab("menus/main/update_sigpatches"_i18n, new ListDownloadTab(contentType::sigpatches, nxlinks));*/
 
->>>>>>> parent of dd85891 (Cleaning the code)
     if (!util::getBoolValue(hideStatus, "firmwares"))
         this->addTab("menus/main/download_firmware"_i18n, new ListDownloadTab(contentType::fw, nxlinks));
 
